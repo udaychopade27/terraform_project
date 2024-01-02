52a784f5f26585385a79196c965c2bfb4ac362c2
Ubuntu <ubuntu@ip-172-31-40-247.ap-south-1.compute.internal>
2024-01-02 08:01:23 +0000

    created aws network resource VPC using IaC

--- main.tf
+++ main.tf
@@ -32,3 +32,18 @@ resource "aws_s3_bucket" "my_s3_bucket" {
        }
 }
 
+resource "aws_vpc" "my_vpc"{
+       cidr_block = "10.0.0.0/16"
+       instance_tenancy = "default"
+       enable_dns_support = "true"
+       enable_dns_hostnames = "true"
+       enable_classiclink = "false"
+       tags = {
+           Name = "MY-VPC"
+       }
+}
+
+
+
+
+

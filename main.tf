be81e0d536eb5888a4e9c3d3f92e2ce72a5c70cb
udaychopade27 <133924961+udaychopade27@users.noreply.github.com>
2024-01-31 11:02:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ terraform {
 
 provider "aws" {
           region = "ap-south-1"
+          # Increase timeout for IMDS request (in seconds)
+          shared_credentials_timeout_seconds = 900
 }
 
 resource "aws_instance" "my_ec2_instance" {
